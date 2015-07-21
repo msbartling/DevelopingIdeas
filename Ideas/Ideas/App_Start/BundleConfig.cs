dec0f4d7cc5d5fabd8ed353926a1eae650c9df74
Matthew <msbartling@hotmail.com>
2015-07-20 22:50:58 -0400

    Added Accordion
    Added jquery ui bundle and css to create a basic accordion on my Ideas
    page

--- Ideas/Ideas/App_Start/BundleConfig.cs
+++ Ideas/Ideas/App_Start/BundleConfig.cs
@@ -8,8 +8,14 @@ namespace Ideas
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+            
+            bundles.UseCdn = true;
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui", @"//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+            
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -26,6 +32,20 @@ namespace Ideas
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                          "~/Content/themes/base/jquery.ui.core.css",
+                          "~/Content/themes/base/jquery.ui.resizable.css",
+                          "~/Content/themes/base/jquery.ui.selectable.css",
+                          "~/Content/themes/base/jquery.ui.accordion.css",
+                          "~/Content/themes/base/jquery.ui.autocomplete.css",
+                          "~/Content/themes/base/jquery.ui.button.css",
+                          "~/Content/themes/base/jquery.ui.dialog.css",
+                          "~/Content/themes/base/jquery.ui.slider.css",
+                          "~/Content/themes/base/jquery.ui.tabs.css",
+                          "~/Content/themes/base/jquery.ui.datepicker.css",
+                          "~/Content/themes/base/jquery.ui.progressbar.css",
+                          "~/Content/themes/base/jquery.ui.theme.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
