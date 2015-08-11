8202a592e442d3382f5d84b99d811291f6e1bec2
Matthew <msbartling@hotmail.com>
2015-08-11 15:32:39 -0400

    Added SignalR tutorial code

--- Ideas/Ideas/Startup.cs
+++ Ideas/Ideas/Startup.cs
@@ -9,6 +9,8 @@ namespace Ideas
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
         }
     }
 }
